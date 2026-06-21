e0ab2b36221038503cfbefa0a5bd3a390556b6e9
y9mfn 💱 <yamin.ahmed.2@city.ac.uk>
2026-06-21 17:29:25 +0100

    Clean AppIcon asset catalog

--- apps/ios/Nima/generate_app_icons.sh
+++ apps/ios/Nima/generate_app_icons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script to generate all iOS app icon sizes from a source image
+# Script to generate the modern single iOS app icon from a source image
 # Usage: ./generate_app_icons.sh <source_image.png>
 
 if [ -z "$1" ]; then
@@ -17,30 +17,8 @@ if [ ! -f "$SOURCE_IMAGE" ]; then
     exit 1
 fi
 
-echo "Generating app icons from $SOURCE_IMAGE..."
+echo "Generating app icon from $SOURCE_IMAGE..."
 
-# iPhone icons
-sips -z 40 40 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-20x20@2x.png"
-sips -z 60 60 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-20x20@3x.png"
-sips -z 58 58 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-29x29@2x.png"
-sips -z 87 87 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-29x29@3x.png"
-sips -z 80 80 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-40x40@2x.png"
-sips -z 120 120 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-40x40@3x.png"
-sips -z 120 120 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-60x60@2x.png"
-sips -z 180 180 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-60x60@3x.png"
-
-# iPad icons
-sips -z 20 20 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-20x20@1x.png"
-sips -z 40 40 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-20x20@2x.png"
-sips -z 29 29 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-29x29@1x.png"
-sips -z 58 58 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-29x29@2x.png"
-sips -z 40 40 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-40x40@1x.png"
-sips -z 80 80 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-40x40@2x.png"
-sips -z 76 76 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-76x76@1x.png"
-sips -z 152 152 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-76x76@2x.png"
-sips -z 167 167 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-83.5x83.5@2x.png"
-
-# App Store icon
 sips -z 1024 1024 "$SOURCE_IMAGE" --out "$ICON_DIR/AppIcon-1024x1024.png"
 
-echo "Done! All app icon sizes have been generated in $ICON_DIR"
+echo "Done! App icon has been generated in $ICON_DIR"
